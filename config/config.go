0ff47009d62a9eddb810126d00f521faa4305943
newlinedeveloper <newlinedeveloper@gmail.com>
2024-07-30 13:09:36 +0530

    CDK application

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	StackName    = "DeveloperSeriesStack"
+	FunctionName = "LambdaFunction"
+	MemorySize   = 128
+	MaxDuration  = 60
+	CodePath     = "function/."
+	Handler      = "bootstrap"
+)
--- developer-series.go
+++ developer-series.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"developer-series/config"
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -18,12 +21,15 @@ func NewDeveloperSeriesStack(scope constructs.Construct, id string, props *Devel
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
-
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("DeveloperSeriesQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	// Create lambda function
+	awslambda.NewFunction(stack, jsii.String(config.FunctionName), &awslambda.FunctionProps{
+		FunctionName: jsii.String(*stack.StackName() + "-" + config.FunctionName),
+		Runtime:      awslambda.Runtime_PROVIDED_AL2(),
+		MemorySize:   jsii.Number(config.MemorySize),
+		Timeout:      awscdk.Duration_Seconds(jsii.Number(config.MaxDuration)),
+		Code:         awslambda.AssetCode_FromAsset(jsii.String(config.CodePath), nil),
+		Handler:      jsii.String(config.Handler),
+	})
 
 	return stack
 }
@@ -33,7 +39,7 @@ func main() {
 
 	app := awscdk.NewApp(nil)
 
-	NewDeveloperSeriesStack(app, "DeveloperSeriesStack", &DeveloperSeriesStackProps{
+	NewDeveloperSeriesStack(app, config.StackName, &DeveloperSeriesStackProps{
 		awscdk.StackProps{
 			Env: env(),
 		},
@@ -42,29 +48,17 @@ func main() {
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
+	account := os.Getenv("CDK_DEPLOY_ACCOUNT")
+	region := os.Getenv("CDK_DEPLOY_REGION")
 
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
+	if len(account) == 0 || len(region) == 0 {
+		account = os.Getenv("CDK_DEFAULT_ACCOUNT")
+		region = os.Getenv("CDK_DEFAULT_REGION")
+	}
 
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	return &awscdk.Environment{
+		Account: jsii.String(account),
+		Region:  jsii.String(region),
+	}
 }
