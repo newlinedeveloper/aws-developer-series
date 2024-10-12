3a835a2be830093a0699701b3ddd8280e2018be4
newlinedeveloper <newlinedeveloper@gmail.com>
2024-10-13 01:14:19 +0530

    Updated eventbridge functionalities

--- developer-series.go
+++ developer-series.go
@@ -28,7 +28,13 @@ func NewDeveloperSeriesStack(scope constructs.Construct, id string, props *Devel
 	// Create the S3 bucket
 	bucket := awss3.NewBucket(stack, jsii.String("ProcessingBucket"), &awss3.BucketProps{
 		RemovalPolicy:      awscdk.RemovalPolicy_DESTROY,
+		BlockPublicAccess:  awss3.BlockPublicAccess_BLOCK_ALL(),
 		EventBridgeEnabled: jsii.Bool(true), // Enable EventBridge integration
+	})
+
+	s3ObjectTaggingStatement := awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Actions:   jsii.Strings("s3:PutObjectTagging"),
+		Resources: jsii.Strings(*bucket.BucketArn() + "/*"),
 	})
 
 	// Create lambda function
@@ -41,42 +47,42 @@ func NewDeveloperSeriesStack(scope constructs.Construct, id string, props *Devel
 		Handler:      jsii.String(config.Handler),
 	})
 
-	// Step 3: Create an EventBridge rule to capture S3 events
-	eventRule := awsevents.NewRule(stack, jsii.String("S3EventRule"), &awsevents.RuleProps{
+	processingLambda.AddToRolePolicy(s3ObjectTaggingStatement)
+	if processingLambda.Role() != nil {
+		processingLambda.Role().AttachInlinePolicy(awsiam.NewPolicy(stack, jsii.String("S3ObjectTaggingPolicy"), &awsiam.PolicyProps{
+			Statements: &[]awsiam.PolicyStatement{s3ObjectTaggingStatement},
+		}))
+	}
+
+	// Create an EventBridge rule for S3 object creation events
+	rule := awsevents.NewRule(stack, jsii.String("S3EventRule"), &awsevents.RuleProps{
 		EventPattern: &awsevents.EventPattern{
 			Source:     jsii.Strings("aws.s3"),
 			DetailType: jsii.Strings("Object Created"),
 			Detail: &map[string]interface{}{
 				"bucket": map[string]interface{}{
-					"name": jsii.Strings(*bucket.BucketName()), // Bucket name must be an array
-				},
-				"object": map[string]interface{}{
-					"key": map[string]interface{}{
-						"prefix": jsii.Strings("order/"), // Prefix must be an array
-					},
+					"name": jsii.Strings(*bucket.BucketName()),
 				},
 			},
 		},
 	})
 
-	// Step 4: Add Lambda function as the target for EventBridge rule
-	eventRule.AddTarget(awseventstargets.NewLambdaFunction(processingLambda, nil))
-
-	// Step 5: Add permission for EventBridge to invoke the Lambda function
-	processingLambda.AddPermission(jsii.String("AllowEventBridgeInvoke"), &awslambda.Permission{
-		Action:    jsii.String("lambda:InvokeFunction"),
-		Principal: awsiam.NewServicePrincipal(jsii.String("events.amazonaws.com"), nil),
-		SourceArn: eventRule.RuleArn(), // Allow invocation only from this rule
-	})
-
-	// Step 6: Add permissions for EventBridge to access S3 events
-	bucket.AddToResourcePolicy(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
-		Actions:   jsii.Strings("s3:PutObject", "s3:GetObject", "s3:ListBucket"),
-		Resources: jsii.Strings(*bucket.BucketArn(), *bucket.BucketArn()+"/*"),
-		Principals: &[]awsiam.IPrincipal{
-			awsiam.NewServicePrincipal(jsii.String("events.amazonaws.com"), nil),
-		},
+	// Add Lambda function as target for the EventBridge rule
+	rule.AddTarget(awseventstargets.NewLambdaFunction(processingLambda, &awseventstargets.LambdaFunctionProps{
+		MaxEventAge:   awscdk.Duration_Hours(jsii.Number(2)),
+		RetryAttempts: jsii.Number(3),
 	}))
+
+	// Output bucket name, Lambda function ARN, and EventBridge rule ARN
+	awscdk.NewCfnOutput(stack, jsii.String("S3BucketName"), &awscdk.CfnOutputProps{
+		Value: bucket.BucketName(),
+	})
+	awscdk.NewCfnOutput(stack, jsii.String("LambdaFunctionARN"), &awscdk.CfnOutputProps{
+		Value: processingLambda.FunctionArn(),
+	})
+	awscdk.NewCfnOutput(stack, jsii.String("EventBridgeRuleARN"), &awscdk.CfnOutputProps{
+		Value: rule.RuleArn(),
+	})
 
 	return stack
 }
